39367f935a040034b228a38c37489fd781fa0b8b
monyatto <atsuuu.0925@gmail.com>
2021-10-17 17:25:00 +0900

    ls syuusei

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,39 +1,39 @@
 # frozen_string_literal: true
 
-class LsCommand
-  def main
-    @files = Dir.entries('.').sort
-    @files = @files.reject { |f| f =~ /^\..*/ }
-    word_count = @files.max_by(&:length).length + 1
-    @column_count = 5
-    @column = 0
-    @line_count = 0
-    (@files.size + @files.size % @column_count).times do
-      if @column == @column_count
+def ls
+    files = Dir.glob("*")
+    word_count = files.max_by(&:length).length + 1
+    column_count = 3
+    column = 0
+    line_count = 0
+    p "column_count: #{column_count}"
+    p "files.size: #{files.size}"
+    p "files: #{files}"
+    p "(files.size.to_f / 2).floor: #{(files.size.to_f / 2).floor}"
+    # ここ
+
+       p "A"
+       gyou = (files.size / column_count.to_f).ceil
+
+       times_count = gyou * column_count
+       # nil_count =  (column_count - retsu ) * gyou - ((files.size / column_count.to_f).floor)
+       # times_count = files.size + nil_count
+
+    p "times_count: #{times_count}"
+    times_count.to_i.times do
+      if column == column_count
         puts "\n"
-        @column = 0
-        @line_count += 1
+        column = 0
+        line_count += 1
       end
-      space_count = word_count - @files[now_column].to_s.size
-      print @files[now_column]
+      now_column = line_count + (files.size / column_count.to_f).ceil.to_i * column
+      space_count = word_count - files[now_column].to_s.size
+      print files[now_column]
       space_count.times { print ' ' }
-      @column += 1
+      print '   '
+      column += 1
     end
     puts "\n"
-  end
-
-  def now_column
-    @line_count + equal_difference * @column
-  end
-
-  def equal_difference
-    if (@files.size % @column_count).zero?
-      @files.size / @column_count
-    else
-      (@files.size / @column_count).floor + 1
-    end
-  end
 end
 
-l = LsCommand.new
-l.main
+  puts ls
