b7d2399c7d28ac4866749eeef01e9cb7059d6d73
monyatto <atsuuu.0925@gmail.com>
2021-10-17 18:02:13 +0900

    ls syuusei2

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,39 +1,19 @@
 # frozen_string_literal: true
 
 def ls
-    files = Dir.glob("*")
-    word_count = files.max_by(&:length).length + 1
-    column_count = 3
-    column = 0
-    line_count = 0
-    p "column_count: #{column_count}"
-    p "files.size: #{files.size}"
-    p "files: #{files}"
-    p "(files.size.to_f / 2).floor: #{(files.size.to_f / 2).floor}"
-    # ここ
+  files = Dir.glob("*")
+  word_count = files.max_by(&:length).length + 4
+  column_count = 8
+  gyou = (files.size / column_count.to_f).ceil
 
-       p "A"
-       gyou = (files.size / column_count.to_f).ceil
-
-       times_count = gyou * column_count
-       # nil_count =  (column_count - retsu ) * gyou - ((files.size / column_count.to_f).floor)
-       # times_count = files.size + nil_count
-
-    p "times_count: #{times_count}"
-    times_count.to_i.times do
-      if column == column_count
-        puts "\n"
-        column = 0
-        line_count += 1
-      end
-      now_column = line_count + (files.size / column_count.to_f).ceil.to_i * column
-      space_count = word_count - files[now_column].to_s.size
-      print files[now_column]
-      space_count.times { print ' ' }
-      print '   '
-      column += 1
+  gyou.times do |index|
+    column_count.times do |count|
+      puts "" if count == (column_count - 1)
+      now_column = index + gyou * count
+      print files[now_column].ljust(word_count) if !files[now_column].nil?
     end
-    puts "\n"
+  end
+  puts ""
 end
 
   puts ls
