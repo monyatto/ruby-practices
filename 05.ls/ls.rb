a6a57f3cc494cc835b847282e4e412c63526163e
monyatto <atsuuu.0925@gmail.com>
2021-10-30 10:43:40 +0900

    ls modify2

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,38 +1,30 @@
 # frozen_string_literal: true
 
 require 'optparse'
+opt = OptionParser.new
+opt.on('-a') do
+  option = File::FNM_DOTMATCH unless ARGV[-1].nil?
+  opt.parse(ARGV)
+end
+
 if !ARGV[-1].nil?
   if Dir.exist?(ARGV[-1])
-    files_path = Dir.glob("#{ARGV[-1]}/*")
-    @files = files_path.map { |f| File.basename(f) }
+    files_path = Dir.glob("#{ARGV[-1]}/*", option)
+    files = files_path.map { |f| File.basename(f) }
   end
 else
   files_path = Dir.glob('*')
-  @files = files_path.map { |f| File.basename(f) }
+  files = files_path.map { |f| File.basename(f), option}
 end
-
-opt = OptionParser.new
-opt.on('-a') do
-  unless ARGV[-1].nil?
-    files_path =
-      if Dir.exist?(ARGV[-1])
-        Dir.glob("#{ARGV[-1]}/*", File::FNM_DOTMATCH)
-      else
-        Dir.glob('*', File::FNM_DOTMATCH)
-      end
-    @files = files_path.map { |f| File.basename(f) }
-  end
-end
-opt.parse(ARGV)
 
 def print_files
-  max_word_count = @files.max_by(&:length).length + 7
+  max_word_count = files.max_by(&:length).length + 7
   column = 3
-  line = (@files.size / column.to_f).ceil
+  line = (files.size / column.to_f).ceil
   line.times do |line_count|
     column.times do |column_count|
       print_file = (line_count + line * column_count)
-      print @files[print_file].ljust(max_word_count) unless @files[print_file].nil?
+      print files[print_file].ljust(max_word_count) unless files[print_file].nil?
       puts '' if column_count == (column - 1)
     end
   end
