2047db89a572ba4b721a821e5fea0aff4e7b0703
monyatto <atsuuu.0925@gmail.com>
2021-11-08 09:19:22 +0900

    modify ls-l

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -34,18 +34,9 @@ def no_option(files)
   print ''
 end
 
-def ftype_decision(l_option, file_stat)
-  ftype = { 'fifo' => 'p', 'characterSpecial' => 'c', 'directory' => 'd', 'blockSpecial' => 'b', 'file' => '-', 'link' => 'l', 'socket' => 's' }
-  l_option << ftype[file_stat.ftype]
-end
-
-def permission_decision(l_option, file_stat_chars)
-  letters = 2
-  3.times do
-    letters += 1
-    permission = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }
-    l_option << permission[file_stat_chars[letters]]
-  end
+def permission_decision(file_stat_chars, letters)
+  permission = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }
+  permission[file_stat_chars[letters]]
 end
 
 def l_option(files)
@@ -54,19 +45,20 @@ def l_option(files)
     path = Pathname.new(File.expand_path(ARGV[0].to_s) << '/' << file)
     file_stat = File::Stat.new(path.relative_path_from(base).to_s)
     l_option = []
-    ftype_decision(l_option, file_stat)
-
+    ftype = { 'fifo' => 'p', 'characterSpecial' => 'c', 'directory' => 'd', 'blockSpecial' => 'b', 'file' => '-', 'link' => 'l', 'socket' => 's' }
+    l_option << ftype[file_stat.ftype]
     file_stat_chars = file_stat.mode.to_s(8).chars
     file_stat_chars.unshift 0 if file_stat_chars.size < 6
-
-    permission_decision(l_option, file_stat_chars)
-
+    letters = 2
+    3.times do
+      letters += 1
+      l_option << permission_decision(file_stat_chars, letters)
+    end
     l_option <<
       ' ' << file_stat.nlink.to_s << ' ' << Etc.getpwuid(File.stat(path).uid).name.to_s <<
       '  ' << Etc.getgrgid(File.stat(path).gid).name.to_s << '  ' << file_stat.size <<
       file_stat.mtime.strftime(' %b %e %H:%M ') << file
-    print l_option.join
-    puts ''
+    puts l_option.join
   end
 end
 
