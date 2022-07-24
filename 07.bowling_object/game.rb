5f2d48b83166b4f2df6a609f607fcf1d99819928
monyatto <atsuuu.0925@gmail.com>
2022-07-24 18:17:20 +0900

    ボウリングのスコア計算プログラムオブジェクト指向版を作成

---
+++ 07.bowling_object/game.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require_relative 'frame'
+
+class Game
+  def initialize(mark)
+    marks = mark.split(',')
+
+    figures = []
+    figures = convert_marks_into_figures(marks, figures)
+
+    scorings = []
+    scorings = convert_figures_into_scoring(figures, scorings)
+
+    @frames = []
+    scorings.each_with_index do |scoring, i|
+      case scoring
+      when :strike
+        @frames[i] = Frame.new(marks[0], marks[1], marks[2])
+      when :spare
+        @frames[i] = Frame.new(marks[0], marks[1], marks[2])
+      when :none
+        @frames[i] = Frame.new(marks[0], marks[1])
+      end
+      marks = remove_marks(marks, scoring)
+    end
+    @frames[9] = Frame.new(marks[0], marks[1], marks[2])
+  end
+
+  def score
+    total = 0
+    @frames.each do |f|
+      total += f.score
+    end
+    total
+  end
+
+  private
+
+  def convert_marks_into_figures(marks, figures)
+    marks.each do |m|
+      if m == 'X'
+        figures.push(10, 0)
+      else
+        figures.push(m.to_i)
+      end
+    end
+    figures
+  end
+
+  def convert_figures_into_scoring(figures, scorings)
+    9.times do
+      scorings <<
+        if figures[0] == 10
+          :strike
+        elsif figures[0] + figures[1] == 10
+          :spare
+        else
+          :none
+        end
+      figures = figures.pop(figures.length - 2)
+    end
+    scorings
+  end
+
+  def remove_marks(marks, scoring)
+    if scoring == :strike
+      marks.pop(marks.length - 1)
+    else
+      marks.pop(marks.length - 2)
+    end
+  end
+end
---
+++ 07.bowling_object/test/main_test.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../main'
+
+class MainTest < Minitest::Test
+  def test_score1
+    mark = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'
+    assert_equal 139, Game.new(mark).score
+  end
+
+  def test_score2
+    mark = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'
+    assert_equal 164, Game.new(mark).score
+  end
+
+  def test_score3
+    mark = '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'
+    assert_equal 107, Game.new(mark).score
+  end
+
+  def test_score4
+    mark = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'
+    assert_equal 134, Game.new(mark).score
+  end
+
+  def test_perfect_game
+    mark = 'X,X,X,X,X,X,X,X,X,X,X,X'
+    assert_equal 300, Game.new(mark).score
+  end
+end
